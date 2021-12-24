23a1aac61d70200b32b3ce275dd5595f4667353e
Seymur <ibragimovs16@mail.ru>
2021-12-25 01:10:28 +0300

    Init.

---
+++ LeetCodeSolving/Easy/1.TwoSum/TwoSum.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeSolving.Easy
+{
+    public static class TwoSumSolve
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j] == target) && i != j) return new int[] { i, j };
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ LeetCodeSolving/Easy/9.PalindromeNumber/PalindromeNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeSolving.Easy
+{
+    public static class PalindromeNumberSolve
+    {
+        public static bool PalindromeNumber(int x)
+        {
+            if (x < 0) return false;
+
+            var strX = x.ToString();
+            var result = "";
+
+            for (int i = strX.Length - 1; i >= 0; i--)
+                result += strX[i];
+
+            if (result == strX) return true;
+
+            return false;
+        }
+    }
+}
